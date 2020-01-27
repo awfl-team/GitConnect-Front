da0191d7f53345e50aeb6b83058833b67ca62a97
lbourgeois <lucasbourgeois60@hotmail.fr>
2020-01-27 18:57:17 +0100

    add vuetify, and start landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import About from '@/views/About.vue'
+import LandingPage from '@/views/LandingPage.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +9,12 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: Home
+        component: LandingPage
     },
     {
         path: '/about',
         name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: (): Promise<typeof import('*.vue')> =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue')
+        component: About
     }
 ]
 
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-container fluid>
+                <v-row>
+                    <v-col cols="12" sm="12" md="6">
+                        <h1>Title</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus et
+                            maiores maiores necessitatibus quam veritatis. Accusamus aliquam,
+                            aperiam beatae, blanditiis corporis culpa facilis officiis possimus
+                            quaerat quia quod repellendus ut vero?
+                        </p>
+                    </v-col>
+                    <v-col cols="12" sm="12" md="6">
+                        <v-tabs>
+                            <v-tab href="#signin">Item One</v-tab>
+                            <v-tab-item value="signin">
+                                <h1>Tab 1 content</h1>
+                            </v-tab-item>
+                            <v-tab href="#signup">Item two</v-tab>
+                            <v-tab-item value="signup">
+                                <h1>Tab 2 content</h1>
+                            </v-tab-item>
+                        </v-tabs>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'landingPage',
+    components: {}
+}
+</script>
